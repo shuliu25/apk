16f1611ccb6b9502aecf745295ba88b21b5e11e3
shuliu25 <2117562347@qq.com>
2026-07-19 11:57:44 +0800

    fix launch theme crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.zishu.personaltrail"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
     }
 
     compileOptions {
